7271d4cce084db9352eb1856df3bd0022977774f
Juan pablo Paredes mendoza <Juan_1320114117_@uptecamac.edu.mx>
2021-09-19 14:35:31 -0500

    Practica_5

---
+++ Practica 5/hola.js
@@ -0,0 +1,14 @@
+document.getElementById("btn-calcular").click(function(){
+    alert();
+});
+
+function calcular(){
+    let num1 = parseInt(document.getElementById("num-1").value);
+    let num2 = parseInt(document.getElementById("num-2").value);
+    var opera = document.getElementById("operacion").value;
+    if(opera == 1){
+        document.getElementById("resultado").value = num1 + num2;
+    }else{
+        document.getElementById("resultado").value = num1 - num2;
+    }
+}       
